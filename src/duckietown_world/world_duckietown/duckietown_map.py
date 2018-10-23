7e8e8ccb65eab1312e189886f08e5c1272d0f384
Andrea Censi <acensi@ethz.ch>
2018-10-23 22:44:58 +0200

    fix

--- src/duckietown_world/world_duckietown/duckietown_map.py
+++ src/duckietown_world/world_duckietown/duckietown_map.py
@@ -6,9 +6,12 @@ class DuckietownMap(PlacedObject):
         self.tile_size = tile_size
         PlacedObject.__init__(self, *args, **kwargs)
 
+    def params_to_json_dict(self):
+        return dict(tile_size=self.tile_size)
+
     def se2_from_curpos(self, cur_pos, cur_angle):
         H = self.children['tilemap'].H
-        gx,gy,gz = cur_pos
+        gx, gy, gz = cur_pos
         p = [gx, (H - 1) * self.tile_size - gz]
         transform = SE2Transform(p, cur_angle)
         return transform
