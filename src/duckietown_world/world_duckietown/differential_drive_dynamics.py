d04c95b416ae10525395c1c21509d2e75dfb7a72
Andrea Censi <acensi@ethz.ch>
2018-11-05 13:48:24 -0500

    done

--- src/duckietown_world/world_duckietown/differential_drive_dynamics.py
+++ src/duckietown_world/world_duckietown/differential_drive_dynamics.py
@@ -16,9 +16,12 @@ __all__ = [
 
 class WheelVelocityCommands(Serializable):
     '''
-        For both, positive = moves forward.
+        This represents the velocity commands for differential drive
+        kinematics.
 
-        Expressed in rad/s.
+        These are expressed in rad/s.
+
+        For both wheels, positive = moves forward.
 
     '''
 
@@ -28,6 +31,13 @@ class WheelVelocityCommands(Serializable):
 
 
 class DifferentialDriveDynamicsParameters(PlatformDynamicsFactory, Serializable):
+    '''
+        This class represents the parameters of the ideal differential drive dynamics.
+
+        radius_left, radius_right: wheels radii
+        wheel_distance: distance between two wheels
+
+    '''
 
     def __init__(self, radius_left, radius_right, wheel_distance):
         self.radius_left = radius_left
@@ -39,23 +49,52 @@ class DifferentialDriveDynamicsParameters(PlatformDynamicsFactory, Serializable)
 
 
 class DifferentialDriveDynamics(GenericKinematicsSE2):
+    """
+        This represents the state of differential drive.
+
+        This is a particular case of GenericKinematicsSE2.
+
+    """
 
     def __init__(self, parameters, c0, t0):
+        """
+        :param parameters:  instance of DifferentialDriveDynamicsParameters
+        :param c0: initial configuration
+        :param t0: initial time
+        """
         check_isinstance(parameters, DifferentialDriveDynamicsParameters)
         self.parameters = parameters
         GenericKinematicsSE2.__init__(self, c0, t0)
 
     def integrate(self, dt, commands):
+        """
+
+        :param dt:
+        :param commands: an instance of WheelVelocityCommands
+        :return:
+        """
         check_isinstance(commands, WheelVelocityCommands)
 
+        # Compute the linear velocity for the wheels
+        # by multiplying radius times angular velocity
         v_r = self.parameters.radius_right * commands.right_wheel_angular_velocity
         v_l = self.parameters.radius_left * commands.left_wheel_angular_velocity
 
-        linear = (v_r + v_l) * 0.5
+        # compute the linear, angular velocities for the platform
+        # using the differential drive equations
+        longitudinal = (v_r + v_l) * 0.5
         angular = (v_r - v_l) / self.parameters.wheel_distance
+        lateral = 0.0
 
-        commands_se2 = geo.se2_from_linear_angular([linear, 0], angular)
+        linear = [longitudinal, lateral]
+
+        # represent this as se(2)
+        commands_se2 = geo.se2_from_linear_angular(linear, angular)
+
+        # Call the "integrate" function of GenericKinematicsSE2
         s1 = GenericKinematicsSE2.integrate(self, dt, commands_se2)
+
+        # new state
         c1 = s1.q0, s1.v0
         t1 = s1.t0
         return DifferentialDriveDynamics(self.parameters, c1, t1)
